c61c7c6d3804e9e373dd447f5e6786d531dec758
WILDWARE-PC\WILDWARE <fazrirahmadrisky@yahoo.co.id>
2016-12-10 08:20:01 +0800

    no message

--- aplikasitoko/aplikasitoko/MainForm.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class MainForm
-
-    Private Sub BarangToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BarangToolStripMenuItem.Click
-        barang.ShowDialog()
-    End Sub
-End Class
