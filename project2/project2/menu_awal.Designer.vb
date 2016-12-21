dfcb3f8e3f79165b0e58396de7501d24b37f18b9
WILDWARE-PC\WILDWARE <fazrirahmadrisky@yahoo.co.id>
2016-12-21 12:30:05 +0800

    form login

---
+++ project2/project2/menu_awal.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class menu_awal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MasterDataToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.but_exit = New System.Windows.Forms.Button()
+        Me.But_ref = New System.Windows.Forms.Button()
+        Me.but_log = New System.Windows.Forms.Button()
+        Me.box_pass = New System.Windows.Forms.TextBox()
+        Me.box_user = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(64, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(385, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "DATA UNIVERSITAS BERSAMA"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MasterDataToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(507, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        Me.MenuStrip1.Visible = False
+        '
+        'MasterDataToolStripMenuItem
+        '
+        Me.MasterDataToolStripMenuItem.Name = "MasterDataToolStripMenuItem"
+        Me.MasterDataToolStripMenuItem.Size = New System.Drawing.Size(78, 20)
+        Me.MasterDataToolStripMenuItem.Text = "Master Data"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.but_exit)
+        Me.GroupBox1.Controls.Add(Me.But_ref)
+        Me.GroupBox1.Controls.Add(Me.but_log)
+        Me.GroupBox1.Controls.Add(Me.box_pass)
+        Me.GroupBox1.Controls.Add(Me.box_user)
+        Me.GroupBox1.Location = New System.Drawing.Point(139, 96)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(235, 182)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Login"
+        '
+        'but_exit
+        '
+        Me.but_exit.Location = New System.Drawing.Point(161, 146)
+        Me.but_exit.Name = "but_exit"
+        Me.but_exit.Size = New System.Drawing.Size(54, 23)
+        Me.but_exit.TabIndex = 4
+        Me.but_exit.Text = "Exit"
+        Me.but_exit.UseVisualStyleBackColor = True
+        '
+        'But_ref
+        '
+        Me.But_ref.Location = New System.Drawing.Point(86, 146)
+        Me.But_ref.Name = "But_ref"
+        Me.But_ref.Size = New System.Drawing.Size(54, 23)
+        Me.But_ref.TabIndex = 3
+        Me.But_ref.Text = "Refresh"
+        Me.But_ref.UseVisualStyleBackColor = True
+        '
+        'but_log
+        '
+        Me.but_log.Location = New System.Drawing.Point(12, 146)
+        Me.but_log.Name = "but_log"
+        Me.but_log.Size = New System.Drawing.Size(54, 23)
+        Me.but_log.TabIndex = 2
+        Me.but_log.Text = "Login"
+        Me.but_log.UseVisualStyleBackColor = True
+        '
+        'box_pass
+        '
+        Me.box_pass.Location = New System.Drawing.Point(35, 89)
+        Me.box_pass.Name = "box_pass"
+        Me.box_pass.Size = New System.Drawing.Size(162, 20)
+        Me.box_pass.TabIndex = 1
+        Me.box_pass.UseSystemPasswordChar = True
+        '
+        'box_user
+        '
+        Me.box_user.Location = New System.Drawing.Point(35, 38)
+        Me.box_user.Name = "box_user"
+        Me.box_user.Size = New System.Drawing.Size(162, 20)
+        Me.box_user.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Username"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Password"
+        '
+        'Timer1
+        '
+        '
+        'menu_awal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 323)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "menu_awal"
+        Me.Text = " "
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MasterDataToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents but_exit As System.Windows.Forms.Button
+    Friend WithEvents But_ref As System.Windows.Forms.Button
+    Friend WithEvents but_log As System.Windows.Forms.Button
+    Friend WithEvents box_pass As System.Windows.Forms.TextBox
+    Friend WithEvents box_user As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ project2/project2/menu_awal.vb
@@ -0,0 +1,26 @@
+﻿Public Class menu_awal
+
+    Private Sub box_user_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles box_user.TextChanged
+
+    End Sub
+
+    Private Sub but_exit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles but_exit.Click
+        End
+    End Sub
+
+    Private Sub but_log_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles but_log.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If box_user.Text = "admin" And box_pass.Text = "1234" Then
+            Timer1.Stop()
+            MsgBox("Selamat Datang", MsgBoxStyle.Information)
+            GroupBox1.Visible = False
+            MenuStrip1.Visible = True
+        Else
+            Timer1.Stop()
+            MsgBox("Login Gagal", MsgBoxStyle.Critical)
+        End If
+    End Sub
+End Class
